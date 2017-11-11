e921eae344143031214cbdb4402a8b653e9738f5
theaschr <schrimpf.alex@gmail.com>
2017-11-11 02:27:02 -0600

    added server phone number error handling

--- backend_connectors/twilio_connector.js
+++ backend_connectors/twilio_connector.js
@@ -2,30 +2,14 @@ var path = require('path');
 var HOME = path.resolve(__dirname+'\\..\\');
 
 var cfg_hndlr = require(HOME+'\\backend_handlers\\config_handler.js');
+var twilio_hndlr = require(HOME+'\\backend_handlers\\twilio_handler.js');
 
 var twilio = require('twilio');
 
 module.exports = function(CONFIG){
 	var module = {};
 
-	module.parse_phone_num = function(phone_num){
-		if(phone_num && phone_num != ""){
-			phone_num = phone_num.match(/(\+)?[0-9]/g);
-			if(phone_num.length == 0){
-				return null;
-			}
-			else if(phone_num[0].length == 2 && phone_num.length == 11){
-				return phone_num.join();
-			}
-			else if(phone_num[0].length == 1 && phone_num.length == 10){
-				return "+1"+ phone_num.join("");
-			}
-			else{
-				return null;
-			}	
-		}
-		return "";
-	}
+
 	module.client = null;
 
 	if(CONFIG){
@@ -46,7 +30,7 @@ module.exports = function(CONFIG){
 			module.client.messages.create({
 				body: message,
 				to: client_phone,
-				from: CONFIG.TWILIO.ACCOUNT_SID.SERVER_PHONE
+				from: twilio_hndlr.parse_phone_number(CONFIG.TWILIO.ACCOUNT_SID.SERVER_PHONE)
 			});
 		}
 
